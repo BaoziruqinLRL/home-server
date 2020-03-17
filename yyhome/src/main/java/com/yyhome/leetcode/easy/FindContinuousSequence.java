6fd486d2b41925e70780d1660d8dd837f9e0f103
miluo <miluo@bxjjren.com>
2020-03-17 14:58:01 +0800

    一比特和二比特

---
+++ yyhome/src/main/java/com/yyhome/leetcode/easy/FindContinuousSequence.java
@@ -0,0 +1,19 @@
+package com.yyhome.leetcode.easy;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author miluo
+ * @date 2020-03-06
+ */
+public class FindContinuousSequence {
+
+    public static void main(String[] args){
+        System.out.println(JSON.toJSONString(findContinuousSequence(9)));
+        System.out.println(JSON.toJSONString(findContinuousSequence(15)));
+    }
+
+    private static int[][] findContinuousSequence(int target) {
+        return null;
+    }
+}
